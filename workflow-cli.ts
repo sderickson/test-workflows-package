54b8940a1b4f6ea6d97eb0b859fd91e9ed7dad6a
Scott Erickson <sderickson@gmail.com>
2025-09-06 08:43:18 -0700

    testing

--- workflow-cli.ts
+++ workflow-cli.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node --experimental-strip-types --disable-warning=ExperimentalWarning
 
-import metaWorkflows from "./package/src/workflows/index.js";
-import { runWorkflowCli } from "./package/src/index.js";
+import metaWorkflows from "saflib-workflows/workflows";
+import { runWorkflowCli } from "saflib-workflows";
+// import { TestWorkflowDefinition } from "./workflows/test.ts";
 
 runWorkflowCli([
   // workflows/add-workflow HOOK - do not remove this line
   ...metaWorkflows,
+  // TestWorkflowDefinition,
 ]);
